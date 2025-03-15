4977c7ee78bc58a08d83d092ba34e6b3b2fb7c24
Ebee02 <beelawrence8@gmail.com>
2025-03-15 08:58:37 +0000

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,16 +4,15 @@ import HeaderLogo from "@/assets/logo.png";
 
 export default function Header() {
   return (
-    <header>
-      <h1>Header Components </h1>
-      <Link href="/">
-        <img src={HeaderLogo.src} alt="header logo" width={20} height={20} />
+    <header className="flex items-center justify-between py-8 px-[1rem] md:px-[10%]">
+      <Link href="/" className="text-2xl font-bold flex gap-5 items-center ">
+        <img src={HeaderLogo.src} alt="header logo" className="w-18 h-18" />
         NextLevel Food
       </Link>
 
       <nav>
-        <ul>
-          <li>
+        <ul className="list-none m-0 p-0 flex gap-[1.5rem] font-[1.5rem]">
+          <li className="text-[#ddd6cb] font-bold py-[0.5rem] px-4 rounded-md">
             <Link href="/meals">Browse Meals</Link>
           </li>
           <li>
