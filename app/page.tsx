562fa0a5c0efe87492a68b7a3e045b0cf265038b
Ebee02 <beelawrence8@gmail.com>
2025-03-22 14:55:11 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,21 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <header className="h-[520px]">
-        <div className=""></div>
-        <div>
-          <div className="">
-            <h1>NextLevel Food for NextLevel Foodies</h1>
-            <p>Taste & share food from all over the world.</p>
+      <header className="h-[520px] flex gap-[3rem] w-[90%]">
+        <div className=" w-[40rem] h-[25rem]  mt-8"></div>
+        <div className="mt-8">
+          <div>
+            <h1 className="text-4xl font-bold font-montserrat tracking-wide uppercase bg-gradient-to-r from-[#f9572a] to-[#ffc905] bg-clip-text text-transparent">
+              NextLevel Food for NextLevel Foodies
+            </h1>
+            <p className="mt-8 mb-10 text-2xl tracking-wide">
+              Taste & share food from all over the world.
+            </p>
           </div>
           <div className="">
-            <Link href="/community">Join the Community</Link>
+            <Link href="/community" className="">
+              Join the Community
+            </Link>
             <Link href="/meals">Explore Meals</Link>
           </div>
         </div>
